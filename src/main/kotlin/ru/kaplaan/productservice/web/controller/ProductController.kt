b88d2bee94ecfd67f4aa1729b3ac28503ece3eda
Sukhareva Sofya <sukhareva@sssofya.ru>
2024-11-16 00:35:07 +0300

    Reformat ProductController

--- src/main/kotlin/ru/kaplaan/productservice/web/controller/ProductController.kt
+++ src/main/kotlin/ru/kaplaan/productservice/web/controller/ProductController.kt
@@ -22,32 +22,41 @@ class ProductController(
 
     @PostMapping
     fun save(
-        @RequestBody @Validated(OnCreate::class)
-        productDto: ProductDto
+      @RequestBody @Validated(OnCreate::class)
+      productDto: ProductDto
     ): ProductDto {
         return productService.save(productDto.toEntity()).toDto()
     }
 
     @GetMapping("/{id}")
-    fun getProductByd(@Valid @Min(1) @PathVariable("id") id: Int): ProductDto {
+    fun getProductByd(
+      @Valid @Min(1) @PathVariable("id") 
+      id: Int): ProductDto {
         return productService.getById(id).toDto()
     }
 
     @GetMapping("/{pageNumber}")
     fun getAllProducts(
-        @PathVariable @Valid @Min(1) pageNumber: Int,
+        @PathVariable @Valid @Min(1) 
+        pageNumber: Int,
+
+        // XXX(sonya): what? and why was it commented?
         //@RequestBody @Valid filters: List<ProductFilter>
     ): List<ProductDto> {
         return productService.getAll(pageNumber).toDto()
     }
 
     @PutMapping
-    fun updateProduct(@RequestBody @Validated(OnUpdate::class) productDto: ProductDto): ProductDto {
+    fun updateProduct(
+      @RequestBody @Validated(OnUpdate::class) 
+      productDto: ProductDto): ProductDto {
         return productService.update(productDto.toEntity()).toDto()
     }
 
     @DeleteMapping("/{id}")
-    fun deleteProductById(@Valid @Min(1) @PathVariable("id") id: Int) {
+    fun deleteProductById(
+      @Valid @Min(1) @PathVariable("id") 
+      id: Int) {
         productService.deleteById(id)
     }
 
